c1fb037751094c408705b8ca750061f6af507b70
xenon615 <xenon615@limeira.eu>
2025-02-20 19:56:14 +0200

    initial

---
+++ examples/basic.rs
@@ -0,0 +1,98 @@
+use std::time::Duration;
+use bevy::{
+    core_pipeline::tonemapping::Tonemapping, pbr::{NotShadowCaster, NotShadowReceiver}, prelude::*, time::common_conditions::on_timer
+};
+use bevy_gltf_animator_helper::{AllAnimations, AniData, AnimatorHelperPlugin};
+
+fn main() {
+    App::new()
+    .insert_resource(ClearColor(Color::BLACK))
+    .add_plugins((
+        DefaultPlugins,
+        AnimatorHelperPlugin
+    ))
+    .add_systems(Startup, startup)
+    .add_systems(Update, switch_animation.run_if(on_timer(Duration::from_secs(5))))
+    .run();
+}
+
+const ANI_COUNT_GIRL: usize = 13;
+const ANI_COUNT_MAN: usize = 5;
+
+fn startup(
+    mut cmd: Commands,
+    mut al : ResMut<AmbientLight>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    assets: ResMut<AssetServer>,
+    mut all_animations: ResMut<AllAnimations>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+
+) {
+    al.brightness = 200.;
+    cmd.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(1., 3., -8.).looking_at(Vec3::ZERO.with_y(1.), Vec3::Y),
+        Tonemapping::ReinhardLuminance
+    ));
+    cmd.spawn((
+        DirectionalLight {
+            illuminance: 5_000.,
+            ..default()
+        },
+        Transform::IDENTITY.looking_at(Vec3::Z, Vec3::Y)
+    ));
+
+    cmd.spawn((
+        Mesh3d(meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(50.)))),
+        MeshMaterial3d(materials.add(Color::BLACK)),
+    ));
+
+    let girl_path = concat!(env!("CARGO_MANIFEST_DIR"), "/examples/assets/girl.glb");
+    let man_path = concat!(env!("CARGO_MANIFEST_DIR"), "/examples/assets/man.glb");
+
+    all_animations.add("Girl", girl_path, ANI_COUNT_GIRL, &mut graphs, &assets);
+    all_animations.add("Man", man_path, ANI_COUNT_MAN, &mut graphs, &assets);
+
+    let sh_girl = assets.load(GltfAssetLabel::Scene(0).from_asset(girl_path));
+    let sh_man = assets.load(GltfAssetLabel::Scene(0).from_asset(man_path));
+
+    let mut x = 0.;
+    for i in 0 .. 3 {
+        x += 1.5 * i as f32 * (if i % 2 == 0 {1.} else {-1.});
+        cmd.spawn((
+            SceneRoot(sh_girl.clone()),
+            NotShadowCaster,
+            NotShadowReceiver,
+            AniData::new("Girl", i),
+            Transform::from_xyz(x, 0., 0.)
+        ));
+    }
+
+
+    let mut x = 0.;
+    let mut z = 0.;
+    for i in 0 .. 4 {
+        x += 2. * i as f32 * (if i % 2 == 0 {1.} else {-1.});
+        z = if i % 2 == 0 {-3.} else {3.};
+        cmd.spawn((
+            SceneRoot(sh_man.clone()),
+            NotShadowCaster,
+            NotShadowReceiver,
+            AniData::new("Man", i),
+            Transform::from_xyz(x, 0., z)
+        ));
+    }
+
+
+}
+
+// ---
+
+fn switch_animation(
+    mut q: Query<&mut AniData>,
+) {
+    for mut ad in &mut q {
+        ad.animation_index = (ad.animation_index + 1) % (if ad.animation_key == "Man" {ANI_COUNT_MAN} else {ANI_COUNT_GIRL});
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,138 @@
+use bevy::prelude::*;
+use bevy::scene::SceneInstanceReady;
+use bevy::utils::HashMap;
+use std::time::Duration;
+
+// ---
+
+#[derive(Event)]
+pub struct AnimationChanged;
+
+
+#[derive(Component)]
+pub struct AniData {
+    pub animation_index: usize,
+    pub player_entity: Entity,
+    pub animation_key: &'static str
+}
+
+impl AniData {
+    pub fn new(key: &'static str, index: usize) -> Self {
+        Self {
+            animation_key: key,
+            animation_index: index,
+            ..default()
+        }
+    }
+}
+
+impl Default for AniData {
+    fn default() -> Self {
+        Self{animation_key: "", animation_index: 0, player_entity: Entity::PLACEHOLDER}
+    }
+}
+
+struct AnimationSet {
+    animations: Vec<AnimationNodeIndex>,
+    graph: Handle<AnimationGraph>,
+}
+
+#[derive(Resource)]
+pub struct AllAnimations(HashMap<&'static str, AnimationSet>);
+
+impl AllAnimations {
+    pub fn add(&mut self, key: &'static str, path: &'static str, count: usize, graphs: &mut ResMut<Assets<AnimationGraph>>, assets: &ResMut<AssetServer>) {
+        if self.0.contains_key(&key) {
+            return;
+        }
+        let mut graph = AnimationGraph::new();
+        self.0.insert(
+            key, 
+            AnimationSet {
+                animations: graph
+                    .add_clips((0..count).map(|i| {assets.load(GltfAssetLabel::Animation(i).from_asset(
+                        path
+                    ))}), 1.0, graph.root)
+                    .collect(),
+                graph: graphs.add(graph),
+            }
+        );
+    }
+}
+
+// ---
+
+pub struct AnimatorHelperPlugin;
+impl Plugin for AnimatorHelperPlugin {
+    fn build(&self, app: &mut App) {
+        app
+        .insert_resource(AllAnimations(HashMap::new()))
+        .add_observer(setup)
+        .add_systems(Update, switch)  // this ?
+        // .add_observer(switch)     // or that ? 
+        ;
+    }
+}
+
+// ---
+
+fn setup(
+    tr: Trigger<SceneInstanceReady>,
+    mut cmd: Commands,
+    all_animations: Res<AllAnimations>,
+    mut akq: Query<&mut AniData>,
+    mut player_q: Query<&mut AnimationPlayer>,
+    children: Query<&Children> 
+) {
+    let parent_e = tr.entity();
+
+    let Ok(mut ani_data) = akq.get_mut(parent_e) else {
+        return;
+    };
+
+    let Some(ani_set) = all_animations.0.get(ani_data.animation_key) else {
+        return;
+    };
+
+    for c in children.iter_descendants(parent_e) {
+        if let Ok(mut player) = player_q.get_mut(c) {
+            // let initial_animation = ani_set.animations.len() - 1;
+            let initial_animation = ani_data.animation_index;
+            let mut transitions = AnimationTransitions::new();
+            transitions
+                .play(&mut player, ani_set.animations[initial_animation] , Duration::ZERO)
+                .repeat()
+            ;
+            cmd
+                .entity(c)
+                .insert(AnimationGraphHandle(ani_set.graph.clone()))
+                .insert(transitions)
+            ;
+            ani_data.player_entity = c;
+            ani_data.animation_index = initial_animation;
+            break;
+        }
+    }    
+}
+
+// ---
+
+pub fn switch(
+    // _tr: Trigger<AnimationChanged>,  // this or that ?
+    mut animation_players: Query<(&mut AnimationPlayer, &mut AnimationTransitions)>,
+    objects_q: Query<&AniData, Changed<AniData>>,
+    all_animations: Res<AllAnimations>,
+) {
+    for adata in objects_q.iter() {
+        if let Ok((mut player, mut transitions)) = animation_players.get_mut(adata.player_entity) {
+            let ani_set = all_animations.0.get(adata.animation_key).unwrap();
+            transitions
+            .play(
+                &mut player,
+                ani_set.animations[adata.animation_index],
+                Duration::from_millis(250),
+            )
+            .repeat();            
+        }
+    }
+}
